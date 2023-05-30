cc14263a458dadfcb563ed35a135cf351f1bce0e
Alinka08 <alinazoldubaeva@gmail.com>
2023-05-30 17:48:12 -0500

    style css update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dogImg, setDogImg] = useState("");
+  const [status, setStatus] = useState("");
+  const [dogList, setDogList] = useState([]);
+
+  // const onClickHandler = () => {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => response.json())
+  //     .then((data) => setDogImg(data.message))
+  //     .catch((err) => console.log(err));
+  // };
+
+  const onClickHandler = async () => {
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const data = await response.json();
+      setDogImg(data.message);
+      setDogList([...dogList, data.message]);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const showDogList = dogList.map((el, index) => {
+    return <img src={el} key={index} alt="dog" className="single-dog" />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="list-container">
+        <h1 className="title">Dog Gallery</h1>
+        <button className="list-btn" onClick={onClickHandler}>
+          Get a dog picture
+        </button>
+        {/* <div>{dogImg && <img src={dogImg} alt="dog" width="400px" />}</div> */}
+        <div>{showDogList}</div>
+      </div>
     </div>
   );
 }
